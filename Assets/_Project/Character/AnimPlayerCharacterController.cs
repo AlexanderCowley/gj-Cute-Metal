e514e1ef67e6fb7fe969115ef9048fa01669f772
mj5045 <mitchelljanek5045@gmail.com>
2022-03-01 22:02:33 -0600

    level one

--- Assets/_Project/Character/AnimPlayerCharacterController.cs
+++ Assets/_Project/Character/AnimPlayerCharacterController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCharacterController : MonoBehaviour
+public class AnimPlayerCharacterController : MonoBehaviour
 {
     public Animator animator;
     CharacterController _characterController;
